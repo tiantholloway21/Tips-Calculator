a7a32633f0501655b37272de88adb3ab3de05699
Tiant D. Holloway <tiantholloway@icloud.com>
2015-12-30 14:11:44 -0600

    Initial Commit

---
+++ Tips Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tips Calculator
+//
+//  Created by Tiant Holloway on 12/30/15.
+//  Copyright © 2015 Tiant Holloway. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
